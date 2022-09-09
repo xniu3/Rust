ad068df38f1774b9ad425878f563ef2374e758b1
xinhaoniu <xniu3@ualberta.ca>
2022-09-09 15:36:46 -0600

    Added Binary Search

---
+++ guess/target/debug/deps/guess-65eb64f81210fd82.d
@@ -0,0 +1,5 @@
+/Users/xinhaoniu/Projects/Rust/guess/target/debug/deps/guess-65eb64f81210fd82.rmeta: src/main.rs
+
+/Users/xinhaoniu/Projects/Rust/guess/target/debug/deps/guess-65eb64f81210fd82.d: src/main.rs
+
+src/main.rs:
---
+++ guess/target/debug/deps/guess-f6854d17a6d35671.d
@@ -0,0 +1,5 @@
+/Users/xinhaoniu/Projects/Rust/guess/target/debug/deps/guess-f6854d17a6d35671.rmeta: src/main.rs
+
+/Users/xinhaoniu/Projects/Rust/guess/target/debug/deps/guess-f6854d17a6d35671.d: src/main.rs
+
+src/main.rs:
